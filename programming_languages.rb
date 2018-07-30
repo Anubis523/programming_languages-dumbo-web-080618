6387bd53283bf3d51248162ab49decb66ccb4151
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-30 12:04:59 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,7 @@ format = {}
       format[language] = type
       format[language][:style] = [languages.keys[index]]
       if ((format[language][:style].nil?) == false && index > 0)
-        
+        format[language][:style] << 
       end
     end
   end
