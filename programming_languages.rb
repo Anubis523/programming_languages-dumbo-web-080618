f3fd23b9acebac75af65daf2c1d55c54d91e3a79
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-30 12:16:35 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ format = {}
     element.each do |language, type|
       format[language] = type
       format[language][:style] = [languages.keys[index]]
-      if (index > 0 && format[language][:style])
+      if (index > 0 && format[language][:style][0] != languages.keys[index])
         format[language][:style] << languages.keys[index]
       end
     end
