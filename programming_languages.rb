853fc75e43a9d59ac0d337f51f1cd5d283a904a4
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-30 12:00:27 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,18 +1,4 @@
 def reformat_languages(languages)
-# empty = {}
-# languages.values.each_with_index do |x,i|
-#   # puts "Value #{x} and index #{i}"
-#   x.each do |language, type|
-#     if (empty.has_key?(language) == false)
-#       # empty[language][:type] = type
-#       empty[language] = type
-#       empty[language][:style] = [languages.keys[i]]
-#     else
-#       empty[language][:style] << languages.keys[i]
-#     end
-#   end
-#   # empty[x] = x
-#   empty
 format = {}
   languages.values.each_with_index do |element, index|
     element.each do |language, type|
