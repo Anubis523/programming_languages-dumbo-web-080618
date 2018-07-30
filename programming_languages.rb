5c09ea0b9fe72d229f21042e9d10aa329776e03f
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-30 12:01:23 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ format = {}
   languages.values.each_with_index do |element, index|
     element.each do |language, type|
       format[language] = type
-      format[language][:style] = [languages.keys[index]]
+      format[language][element] = [languages.keys[index]]
     end
   end
 format
