96caabafcec0f2fa27bb13e7360e3119f85e9bc0
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-30 12:00:01 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -16,8 +16,8 @@ def reformat_languages(languages)
 format = {}
   languages.values.each_with_index do |element, index|
     element.each do |language, type|
-        format[language] = type
-        format[language][:style] = [languages.keys[index]]
+      format[language] = type
+      format[language][:style] = [languages.keys[index]]
     end
   end
 format
