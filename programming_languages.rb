3181fc156241077083fe7296f5f39f230dd976ed
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-30 12:20:32 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
 format = {}
   languages.values.each_with_index do |element, index|
     element.each do |language, type|
-      if(index == 0)
+      if(format[language][:style].nil?)
         format[language] = type
       format[language][:style] = [languages.keys[index]]
       elsif (format[language][:style][0] != languages.keys[index])
